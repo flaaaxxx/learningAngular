53c7c9c0144dcd9684a6ab55374bbd27b9d58bca
developer <lukasz.szysz@pentacomp.pl>
2026-04-23 06:58:42 +0200

    Dodanie komponentu user, routing

---
+++ src/app/users/users/users.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  imports: [],
+  templateUrl: './users.html',
+  styleUrl: './users.css',
+  standalone: true
+})
+export class Users {}
